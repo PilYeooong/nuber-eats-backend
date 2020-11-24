a10b8603160b2dffa4a617eff91b6072219fad42
PilYeong <pilyeooong@gmail.com>
2020-11-24 15:05:25 +0900

    ADD: connect DB using TypeORM, add restaurant entity, dto, services

---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,39 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+import { IsBoolean, IsString, Length } from "class-validator";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+// @InputType({ isAbstract: true })
+@ObjectType()
+@Entity()
+export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(() => Number)
+  id: number;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  @Length(5, 10)
+  name: string;
+
+  @Field(() => Boolean)
+  @Column()
+  @IsBoolean()
+  isVegan: boolean;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  address: string;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  ownersName: string;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  categoryName: string;
+
+}
---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,25 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
+
+@Resolver(() => Restaurant)
+export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+  @Query(() => [Restaurant])
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
+  }
+  @Mutation(() => Boolean)
+  async createRestaurant(
+    @Args('input') createRestaurantDto: CreateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantService.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
