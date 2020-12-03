1220734fbd55c2ef8fcf271fb2f7c6ae168deb20
PilYeong <pilyeooong@gmail.com>
2020-12-03 17:38:11 +0900

    ADD: user e2e test

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -26,6 +26,13 @@ describe('UserModule (e2e)', () => {
   let verificationRepository: Repository<Verification>;
   let jwtToken: string;
 
+  const baseTest = () => request(app.getHttpServer()).post(GRAPHQL_ENDPOINT);
+  const publicTest = (query: string) => baseTest().send({ query });
+  const privateTest = (query: string) =>
+    baseTest()
+      .set('x-jwt', jwtToken)
+      .send({ query });
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -46,22 +53,19 @@ describe('UserModule (e2e)', () => {
 
   describe('createAccount', () => {
     it('should create account', () => {
-      return request(app.getHttpServer())
-        .post(GRAPHQL_ENDPOINT)
-        .send({
-          query: `
-        mutation {
+      return publicTest(
+        `mutation {
           createAccount(
-            input: { 
-              email: "${testUser.email}"
-              password: "${testUser.password}"
-              role: Client 
-            }) {
+          input: { 
+            email: "${testUser.email}"
+            password: "${testUser.password}"
+            role: Client 
+          }) {
             ok
             error
           }
         }`,
-        })
+      )
         .expect(200)
         .expect(res => {
           expect(res.body.data.createAccount.ok).toBe(true);
@@ -163,22 +167,17 @@ describe('UserModule (e2e)', () => {
     });
 
     it('should see a user profile', () => {
-      return request(app.getHttpServer())
-        .post(GRAPHQL_ENDPOINT)
-        .set('x-jwt', jwtToken)
-        .send({
-          query: `
-        {
-          userProfile(userId: ${userId}) {
-            ok
-            user {
-              id
-              email
-            }
+      return privateTest(`
+      {
+        userProfile(userId: ${userId}) {
+          ok
+          user {
+            id
+            email
           }
         }
-        `,
-        })
+      }
+      `)
         .expect(200)
         .expect(res => {
           const {
